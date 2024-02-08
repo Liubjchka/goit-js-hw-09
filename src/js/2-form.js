3e5d5a360cc2cd0dd33f0861f6f07c2caa6a71ef
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-02-08 23:37:56 +0200

    add 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,6 +6,16 @@ formEl.addEventListener('input', event => {
   const formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
   formData[name] = value.trim();
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+
+  const allFieldsFilled = Array.from(formEl.elements).every(
+    field => field.value.trim() !== ''
+  );
+
+  if (allFieldsFilled) {
+    formEl.querySelector('button').removeAttribute('disabled');
+  } else {
+    formEl.querySelector('button').setAttribute('disabled', 'disabled');
+  }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,6 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
   new FormData(formEl).forEach((value, key) => {
     dataObj[key] = value;
   });
+  const allFieldsFilled = Array.from(formEl.elements).every(
+    field => field.value.trim() !== ''
+  );
+
+  if (allFieldsFilled) {
+    formEl.querySelector('button').removeAttribute('disabled');
+  } else {
+    formEl.querySelector('button').setAttribute('disabled', 'disabled');
+  }
 });
 
 formEl.addEventListener('submit', event => {
@@ -21,4 +40,5 @@ formEl.addEventListener('submit', event => {
   console.log(formDataEl);
   localStorage.removeItem(LOCAL_STORAGE_KEY);
   formEl.reset();
+  formEl.querySelector('button').setAttribute('disabled', 'disabled');
 });
